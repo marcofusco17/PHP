c0c578e870f14bb527fce9e9c9f55fbd4f04972c
marcofusco17 <fusco.marco17@gmail.com>
2021-10-23 13:58:11 +0200

    Practica 2
    Practica 2

--- Practica2/practica.php
+++ Practica2/practica.php
@@ -1,28 +1,27 @@
 <?php
 
-/* bucle que cuenta del 0 al 9
+
 for ($counter= 0; $counter < 10; $counter++){
 
     echo $counter; 
 }
-*/
 
-/* un bucle  infinito 
+
+ 
 for (;;){
     echo "infinito";
 }
-*/
 
 
-/*contar de dos en dos 
+
+
 
 for ($counter= 0; $counter < 10; $counter= $counter +2){
 
     echo $counter; 
 }
-*/
 
- /*funcion de suma 
+
 function suma ($num1, $num2){
     return $num1 + $num2 ;
 
@@ -30,12 +29,12 @@ function suma ($num1, $num2){
 
 $total = suma(1,2);
 echo $total
-*/
 
-/* funcion que junta dos cosas 
+
+
 function concatenar ($palabra1, $palabra2){
     return $palabra1.$palabra2; 
 }
 echo concatenar ("hola", " Julian");
-*/
+
 ?>
